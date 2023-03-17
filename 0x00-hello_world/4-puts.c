94395f26babc20165efbbd06a976877276ac77aa
abdelilahboubekri <abdelilahbob@gmail.com>
2023-03-16 18:58:16 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Programming is very nice and cool
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
