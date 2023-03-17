94aecbd919b02ecdc222fa98de2d3d2cf8175154
abdelilahboubekri <abdelilahbob@gmail.com>
2023-03-16 19:53:53 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 /**
- * main - program that prints the size of various types on the computer
- * Return (0) (Success)
+ * main - Entry point
+ * Description: using sizeof to print the size of various types.
+ * Return: (0) (Success)
  */
+
 int main(void)
 {
 	char a;
