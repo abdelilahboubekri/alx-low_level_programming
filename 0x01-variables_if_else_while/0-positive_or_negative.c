919288b6d0bf9d180bdde58827d2e7e5005532e6
abdelilahboubekri <abdelilahbob@gmail.com>
2023-03-17 19:44:03 -0700

    yes

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,4 +1,4 @@
-i#include <stdlib.h>
+#include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 /* more headers goes there */
@@ -21,6 +21,6 @@ else if (n == 0)
 printf("%d is zero\n", n);
 else
 	printf("%d is negative\n", n);
-printf("\n"
+printf("\n");
 return (0);
 }
