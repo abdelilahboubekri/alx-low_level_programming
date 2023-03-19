88b92ae381479f3b32cf391033fdb41f2c2f4064
abdelilahboubekri <abdelilahbob@gmail.com>
2023-03-19 04:24:07 -0700

    es

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,7 +10,7 @@
 int main(void)
 {
 	int n;
-	srand(time(0));
+	srand (time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
@@ -24,6 +24,5 @@ int main(void)
 	{
 		printf("%d is negative\n", n);
 	}
-	return (0);
+return (0);
 }
-
