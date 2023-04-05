82d10337153b9eeef9f475ecdcaea355777144d4
abdelilahboubekri <abdelilahbob@gmail.com>
2023-04-05 15:05:46 -0700

    print rev recusion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * _puts_recursion - function like puts();
- * @s: input
+ * _puts_recursion -  function that prints a string, followed by a new line
+ * @s: string to be printed
  * Return: Always 0 (Success)
  */
 void _puts_recursion(char *s)
