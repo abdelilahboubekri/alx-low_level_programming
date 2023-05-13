0d96e4c70e9410c303decbd13c1a1b409b695766
abdelilahboubekri <abdelilahbob@gmail.com>
2023-05-12 19:56:36 -0700

    h

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,20 +11,27 @@
   */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t bytes;
+	ssize_t a, b;
+	char *buf;
 	int fd;
-	char buf[READ_BUF_SIZE * 8];
 
-	if (!filename || !letters)
+	if (!filename)
 		return (0);
-	fd = open(filename, 0_RDONLY);
+
+	fd = open(filename, O_RDONLY);
 
 	if (fd == -1)
 		return (0);
 
-	bytes = read(fd, &buf[0], letters);
-	bytes = write(STDOUT_FILENO, &buf[0], bytes);
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
+
+	a = read(fd, buf, letters);
+    b = write(STDOUT_FILENO, buf, a);
+
 	close(fd);
 
-	return (bytes);
-}
+	free(buf);
+
+	return (b);
