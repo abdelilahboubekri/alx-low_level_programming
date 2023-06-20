3eecd9ba3d03cecfa06f4544e1450b2bb4074614
abdelilahboubekri <abdelilahbob@gmail.com>
2023-06-19 17:28:36 -0700

    libdynamic

---
+++ 0x18-dynamic_libraries/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * main - Entry the point
+ *
+ * Description : print _putchar by using prototype
+ *
+ * Return: 0
+ */
+
+
+int main(void)
+
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
